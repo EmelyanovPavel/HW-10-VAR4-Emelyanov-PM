8b189cfaf2c5a0520efe2d16aab9dc1b6f2e03b8
EmelyanovPavel <gdetoty@list.ru>
2025-05-08 12:09:54 +0300

    Update Strings2.cpp
    Написан код с использованием класса std::string, цикл for изменен на цикл while в теле функции, возвращающей булево значение.

--- Strings2.cpp
+++ Strings2.cpp
@@ -1,43 +1,66 @@
 //Homework 10.
-#include <algorithm>
 #include <iostream>
-#include <sstream>
 #include <string>
 
-//Task 1.
 //Complex actions with strings.A meaningful text message is given(i.e.
 //    alphanumeric information separated by spaces and
 //    punctuation marks, with a dot at the end).Perform the work
 //    using the std::string class.
-//4. Display all the palindrome words contained in the specified
-//message.
+//4. Display all the palindrome words contained in the specified message.
 
-bool isPalindrome(std::string str)
+bool isPalindrome(std::string word) 
 {
-    for (int left = 0, right = str.length() - 1; left < right; left++, right--) //go to the middle
-        if (str[left] != str[right]) //check for symmetry
+    std::string::const_iterator left = word.begin();
+    std::string::const_iterator right = word.end() - 1;
+    
+    while (left < right) 
+    {
+        if (*left != *right) 
+        {
             return false;
+        }
+        left++;
+        right--;
+    }
     return true;
 }
 
-void task1()
+void task1() 
 {
-    //declaring a variable
-    std::string str;
-
-    //getting a string
-    std::getline(std::cin, str);
-
-    //turning a string into a stream
-    std::istringstream ist(str);
-
-    while (ist >> str) {
-        //reading the stream
-        if (isPalindrome(str)) { //finding a palindrome
-            std::cout << str << " "; //output a palindrome words
+    std::string message;
+    std::cout << "Enter the string: ";
+    std::getline(std::cin, message);
+    
+    std::string currentWord = "";
+    bool isWord = false;
+    
+    std::cout << "The palindrome words: " << std::endl;
+    
+    int i = 0;
+    while (i < message.length()) 
+    {
+        if (isalpha(message[i])) 
+        {
+            currentWord += tolower(message[i]);
+            isWord = true;
         }
+        else if (isWord) 
+        {
+            if (isPalindrome(currentWord)) 
+            {
+                std::cout << currentWord << std::endl;
+            }
+            currentWord = "";
+            isWord = false;
+        }
+        i++;
     }
-    std::cout << std::endl;
+    
+    //Checking the last word
+    if (isWord && isPalindrome(currentWord)) 
+    {
+        std::cout << currentWord << std::endl;
+    }
 }
 
 int main()
